981af99e6f61a08e0520b04eb5c4291b63a06dda
Roberthall653 <126697637+Roberthall653@users.noreply.github.com>
2023-07-14 08:27:58 -0700

    took out my MySQL password in the connection call

--- questions.js
+++ questions.js
@@ -5,7 +5,7 @@ const db = mysql.createConnection(
   {
     host: 'localhost',
     user: 'root',
-    password: 'Amdrum12!',
+    password: '',
     database: 'company_db'
   },
   console.log(`Connected to the company_db database.`)
